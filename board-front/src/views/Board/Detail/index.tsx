08a44f1784ef9ad0eb62136fdc50559feeca2f82
kjwjwjw <tbs03112@naver.com>
2024-05-28 21:26:14 +0900

    상세게시물

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -4,8 +4,57 @@ import './style.css';
 //      component : 게시물 상세 화면 컴포넌트   //
 export default function BoardDetail() {
 
+  //      component : 게시물 상세 상단 컴포넌트   //
+  const BoardDetailTop = () => {
+
+    //      render : 게시물 상세 상단 컴포넌트 렌더링  //  
+    return (  
+      <div id='board-detail-top'>
+        <div className='board-detail-top-header'>
+          <div className='board-detail-title'>{'점심메뉴 추천'}</div>
+          <div className='board-detail-top-sub-box'>
+            <div className='board-detail-write-info-box'>
+              <div className='board-detail-writer-profile-image'></div>
+              <div className='board-detail-writer-nickname'>{'안녕하세요'}</div>
+              <div className='board-detail-info-divider'>{'\|'}</div>
+              <div className='board-detail-writer-date'>{'2024. 05. 28'}</div>
+            </div>
+            <div className='icon-button'>
+              <div className='icon more-icon'></div>
+            </div>
+            <div className='board-detail-more-box'>
+              <div className='board-detail-update-button'>{'수정'}</div>
+              <div className='divider'></div>
+              <div className='board-detail-delete-button'>{'삭제'}</div>
+            </div>
+          </div>
+        </div> 
+        <div className='divider'></div> 
+        <div className='board-detail-top-main'>
+          <div className='board-detail-main-text'>{'돼지고기, 소고기'}</div>
+          <div className='board-detail-main-image'></div>
+        </div> 
+      </div>
+    
+    );
+  };
+  //      component : 게시물 상세 하단 컴포넌트   //
+  const BoardDetailBottom = () => {
+
+    //      render : 게시물 상세 하단 컴포넌트 렌더링  //  
+    return (<></>);
+  };
+
 //      render : 게시물 상세 화면 컴포넌트 렌더링  //  
   return (
-    <div>게시물 상세보기</div>
+    <div id='board-detail-wrapper'>
+      <div className='board-detail-container'>
+        <BoardDetailTop/>
+        <BoardDetailBottom/>
+
+      </div>
+
+
+    </div>
   )
 }
